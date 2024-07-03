42a402f8646545635b51f47820a227a1f0b91ae9
maelamustara <m.mustara@campus.tu-berlin.de>
2024-07-03 21:32:22 +0200

    Neue Änderung an solver.jl

--- src/solver.jl
+++ src/solver.jl
@@ -1,34 +1,37 @@
 #=
-Das Funktioniert auch nicht :D es gibt ein Problem bei den Richtungen, in denen der path gefolgt wird
+Neue Änderung nach der Rechnerbetreuung. Es funktioniert immer noch nicht aber es ist näher an das Endprogramm
 =#
-function solving_labyrinth(start::Node,exit::Node,mat::Matrix{Node})::Vector{Node}
+include("core.jl")
+
+function solving_labyrinth(mat::Matrix{Node})::Tuple{Node,Node,Vector}
+    start = rand(mat) #??
+    exit = rand(mat) #??
     if start.key === exit.key
-        return [start,exit]
+        return start,exit,[start,exit]
     end
     start_idx = Tuple(findfirst(x -> x.key === start.key,mat))
     exit_idx = Tuple(findfirst(x -> x.key === exit.key,mat))
-    path = [start]
+    path = [(start,nothing)]
     i,j = start_idx
-    println(start_idx)
-    println(exit_idx)
-    while (i,j) !== exit_idx
-        println(i,j)
-        if mat[i,j].right !== nothing
-            i,j = i,j+1
-            push!(path,mat[i,j])
-        elseif mat[i,j].up !== nothing
-            i,j = i-1,j
-            push!(path,mat[i,j])
-        elseif mat[i,j].left !== nothing
-            i,j = i,j-1
-            push!(path,mat[i,j])
-        elseif mat[i,j].down !== nothing
-            i,j = i+1,j
-            push!(path,mat[i,j])
+    d = 1
+    directions = [(0,1),(-1,0),(0,-1),(1,0)]
+    while (i,j) !== exit_idx # bis wir das ziel finden
+        while true # bis wir eine mögliche richtung finden
+            # können wir in direction d gehen? break
+            if mat[i+directions[d][1],j+directions[d][2]] ∈ neighbours(mat[i,j])
+                break
+            end
+            d = d%4 + 1
         end
+        i += directions[d][1]
+        j += directions[d][2]
+        push!(path,(mat[i,j],d)) 
+        d = path[end][2]
     end
-    return path
+    return start,exit,path
 end
+
+
 
 
 
